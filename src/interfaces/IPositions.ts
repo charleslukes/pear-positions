ba5d713b83f00e5a2e6cfad94a9e6374af65b6c1
charleslukes <charleslukes28@gmail.com>
2023-10-18 13:30:11 +0100

    add position interface

---
+++ src/interfaces/IPositions.ts
@@ -0,0 +1,9 @@
+export interface Params {};
+export interface ResBody {};
+export interface ReqBody {};
+
+export interface ReqQuery {
+  account: string;
+  showlPnlAfterFees?: string;
+  isPnlInLeverage?: string;
+}
