2a638e21a951106dd3d8be897ab9ec3c2d9453f2
charleslukes <charleslukes28@gmail.com>
2023-10-18 13:31:10 +0100

    add entry for web3

---
+++ web3/index.ts
@@ -0,0 +1,8 @@
+import { getAllPositions } from "./src/positions";
+import { tokenInfo, getDetailedTokenInfos } from "./src/tokenInfo";
+
+export default {
+  getAllPositions,
+  tokenInfo,
+  getDetailedTokenInfos,
+};
