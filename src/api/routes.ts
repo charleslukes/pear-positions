74672b58209ad42911121bd51db24e8c53722162
charleslukes <charleslukes28@gmail.com>
2023-10-18 14:08:22 +0100

    update routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { getAccountPositions } from "./position";
+import { getAccountTokenInfo } from "./tokenInfo";
 const router = express.Router();
 
 // All routes are defined here
-router.route("/").get(getAccountPositions);
+router.route("/positions").get(getAccountPositions);
+router.route("/tokens-info").get(getAccountTokenInfo);
 
 export default router;
